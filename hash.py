2ff0d985dd8bdd3ae9a318e414c49930ee7bbe41
DanIzunna <dannwahiri@gmail.com>
2022-09-16 15:32:05 +0100

    deleted 'hash.py' used to test hashing

--- hash.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-string=1224
-string = str(string)
-encoded=string.encode()
-result = hashlib.sha256(encoded)
-print("String : %s"%string, end ="")
-print("\nHexadecimal equivalent: ",result.hexdigest())
