68ea2ab6bfc57df92d57d4c259e49a9a750a80a4
DanIzunna <dannwahiri@gmail.com>
2022-09-17 03:45:59 +0100

    Updated database

--- BANK.sql
+++ BANK.sql
@@ -6,9 +6,7 @@ select * from customers;
 -- ACCOUNT_NUMBER INT NOT NULL, ACCOUNT_TYPE VARCHAR(64) NOT NULL,  BALANCE float, CURRENCY VARCHAR(3), DATE_CREATED date NOT NULL);
 
 -- drop table customers;
--- DELETE FROM customers WHERE (ID_NO between 100 and 200 );
+-- DELETE FROM customers WHERE (ID_NO between 21 and 23 );
 
 -- select * from customers where account_number = 272261537 and pin = 'cbfad02f9ed2a8d1e08d8f74f5303e9eb93637d47f82ab6f1c15871cf8dd0481';
 -- DELETE FROM customers WHERE account_number = 272261537 and pin =  'cbfad02f9ed2a8d1e08d8f74f5303e9eb93637d47f82ab6f1c15871cf8dd0481'
-
-DELETE FROM customers WHERE account_number = 274408858 and pin = 'cbfad02f9ed2a8d1e08d8f74f5303e9eb93637d47f82ab6f1c15871cf8dd0481'
