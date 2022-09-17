79ff71636e0a75931591bd804dbf35a7a69ec931
DanIzunna <dannwahiri@gmail.com>
2022-09-17 03:41:46 +0100

    Changer '.upper() to '.capitalize()'

--- name_check.py
+++ name_check.py
@@ -4,7 +4,7 @@ def first_name_check():
     first_name = input('Please Enter your First Name: ')
     time.sleep(0.5)
     if first_name.isalpha() == True:
-        first_name = first_name.upper()
+        first_name = first_name.capitalize()
     else:
         print('Invalid Name!!')
         return first_name_check()
@@ -13,7 +13,7 @@ def first_name_check():
 def last_name_check():
     last_name = input('Please Enter your  Last Name: ')
     if last_name.isalpha() == True:
-        last_name = last_name.upper()
+        last_name = last_name.capitalize()
     else:
         print('Invalid Name!!')
         return last_name_check()
