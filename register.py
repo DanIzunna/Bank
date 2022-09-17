821073a9eb3fc8811b6790b9384c7416266b3154
DanIzunna <dannwahiri@gmail.com>
2022-09-17 03:45:12 +0100

    changed pin to check_pin

--- register.py
+++ register.py
@@ -1,7 +1,7 @@
 import datetime
 import random
-import hashlib
 from name_check import *
+from check_pin import *
 from date_of_birth import date_of_birth_check
 from gender_check import gender_check
 from current_acc import Current_Account
@@ -64,30 +64,3 @@ class Register:
         cursor.execute(sql, vals)
         mydb.commit()
         return ('Account Successfully created')
-        
-#Make password confirmation
-def pin_check():
-    _pin = (input('Create your FOUR digit secret PIN: '))
-    if _pin.isnumeric() == True and len(_pin) == 4:
-        _pin = int(_pin)
-        
-        raw_pin = input('Confirm your PIN: ')
-        if raw_pin.isnumeric() == True and len(raw_pin) == 4:
-            raw_pin = int(raw_pin)
-        if raw_pin == (_pin):
-        # Hashing the pin for enhanced security
-            pin_hash = str(raw_pin)
-            encoded=pin_hash.encode()
-            pin = hashlib.sha256(encoded).hexdigest()
-            return pin
-        elif raw_pin != _pin:
-            print('Passwords don\'t match!')
-            return pin_check()
-
-    elif _pin.isnumeric() != True:
-        print('Invalid !!\npin Must be numbers only!!'.upper())
-        return pin_check()
-
-    elif _pin.isnumeric() == True and len(_pin) > 4:
-        print('Invalid !!\npin Must be four digits only!!'.upper())
-        return pin_check()
