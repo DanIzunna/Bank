12c844279d38d9c936d34cd9fe563f6a00272b19
DanIzunna <dannwahiri@gmail.com>
2022-09-19 02:42:54 +0100

    Changed control flow of pin_check

--- pin_check.py
+++ pin_check.py
@@ -2,20 +2,22 @@ import hashlib
 
 # Separating the pin check for ease of access
 def pin_check():
+        
     raw_pin = (input('Enter your FOUR digit PIN: '))
-    if raw_pin.isnumeric() == True and len(raw_pin) == 4:
-        raw_pin = int(raw_pin)
-
-    # Hashing the pin for enhanced security
-        pin_hash = str(raw_pin)
-        encoded=pin_hash.encode()
-        pin = hashlib.sha256(encoded).hexdigest()
-
-    elif raw_pin.isnumeric() != True:
+    if raw_pin.isnumeric() != True:
         print('Invalid !!\npin Must be numbers only!!'.upper())
         return pin_check()
 
     elif raw_pin.isnumeric() == True and len(raw_pin) > 4:
         print('Invalid !!\npin Must be four digits only!!'.upper())
         return pin_check()
-    return pin
+        pin = pin
+
+    elif raw_pin.isnumeric() == True and len(raw_pin) == 4:
+        raw_pin = int(raw_pin)
+    # Hashing the pin for enhanced security
+        pin_hash = str(raw_pin)
+        encoded=pin_hash.encode()
+        pin = hashlib.sha256(encoded).hexdigest()
+        return pin
+    
