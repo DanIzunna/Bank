ab8102d13483dd6c3dd900e4cd3e7724c9901929
DanIzunna <105650690+DanIzunna@users.noreply.github.com>
2022-10-07 11:11:44 +0100

    Update sql_connect.py
    Editing MySQL auth details

--- sql_connect.py
+++ sql_connect.py
@@ -1,5 +1,5 @@
 import mysql.connector
-mydb = mysql.connector.connect(user="Jon",passwd="ilikecheeseballs2",host="localhost",database ='BANK', auth_plugin= 'mysql_native_password' ) 
+mydb = mysql.connector.connect(user={ your_mysql_user_name },passwd= { your_mysql_password },host="localhost",database ='BANK', auth_plugin= 'mysql_native_password' ) 
 cursor = mydb.cursor()
 
 # The commands to connect to the database is in one file for ease of access
